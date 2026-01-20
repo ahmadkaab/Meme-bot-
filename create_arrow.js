92c6fcc59a5075aeca3a72f4b50ac26fdf2dd2c3
Ahmad kaab <155531733+ahmadkaab@users.noreply.github.com>
2026-01-21 04:34:26 +0530

    chore: remove remaining compilation assets (arrow graphics)

--- create_arrow.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const buffer = Buffer.from(fs.readFileSync('arrow_base64.txt', 'utf-8'), 'base64');
-fs.writeFileSync('arrow.png', buffer);
-console.log('Arrow asset created.');
